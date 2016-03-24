b6fa8b4880946da4c5f0e028c030a7f902db6356
Onur Uyar <me@onuruyar.com>
2016-03-24 16:21:13 +0000

    fix thumb column styles

--- webpack/javascripts/pc-thumb.js
+++ webpack/javascripts/pc-thumb.js
@@ -5,7 +5,7 @@ import riot from 'riot';
 riot.tag('pc-thumb',
 
   `
-  <div class="col col-3 center">
+  <div class="sm-col sm-col-3 center p1">
     <a class="block p2 bg-white rounded" href="{ parent.opts.resource }/{ id }/edit" title="{ parent.opts.resource } / edit" onclick="{ parent.opts.navigate }">
       <i class="fa fa-file fa-2x"></i>
       <h6 class="break-word">{ title }<br><small>/{ pathname }</small></h6>
