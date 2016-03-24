e729697aeca5d5e0f483b114f144f1fb45f6205d
Onur Uyar <me@onuruyar.com>
2016-03-24 16:01:44 +0000

    fix js/css rendering

--- app/helpers/papercat/pages_helper.rb
+++ app/helpers/papercat/pages_helper.rb
@@ -6,16 +6,16 @@ module Papercat
       body = page.body
       ERB.erb_implementation.new(page.body).result(binding).html_safe
     end
-    
+
     def stylesheets
       content_tag :style do
-        Stylesheet.all.inject('') { |m, s| m + s.body }
+        Stylesheet.all.inject('') { |m, s| m + s.body }.html_safe
       end
     end
-    
+
     def scripts
       content_tag :script do
-        Javascript.all.inject('') { |m, s| m + s.body }
+        Javascript.all.inject('') { |m, s| m + s.body }.html_safe
       end
     end
   end
